97ba1231c45194255b793a5fc81162f38a94631b
Brett Bonnet <brettbonnet4@gmail.com>
2019-08-31 12:27:38 -0400

    started #build_tree

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,2 +1,25 @@
 class BinarySearchTree
+    def initialize(array=nil)
+        @array = array
+        @root = nil
+        build_tree(@array)
+    end
+
+    def build_tree(array)
+        counter = 0
+        while counter <= array.length do
+            node = Node.new
+            current = node
+            next_node = node.child
+            previous_node = node.parent
+            node.value = array[counter]
+            if counter == 0
+                @root = node
+                
+            else
+            end
+            counter += 1
+        end
+    end
+
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
+require './lib/node.rb'
+require './lib/binary_search_tree.rb'
+
+array = [1, 2, 3, 4, 5]
+
+new_tree = BinarySearchTree.new(array)
+
+
 
