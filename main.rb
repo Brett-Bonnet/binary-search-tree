2a10d120f3f88d2b30c73b24b028ea86d94a0931
Brett Bonnet <brettbonnet4@gmail.com>
2019-09-05 15:31:58 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,14 @@
+    
 require './lib/node.rb'
 require './lib/binary_search_tree.rb'
 
-array = [1, 2, 3, 4, 5]
+array = [100, 200, 300, 150, 250, 5000, 100, 10, 3, 5, 6000]
 
 new_tree = BinarySearchTree.new(array)
+
+puts new_tree.breadth_first_search(5000)
+puts new_tree.depth_first_search(5)
+puts new_tree.dfs_rec(150)
 
 
 
